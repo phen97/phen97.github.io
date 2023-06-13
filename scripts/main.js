4e0b80eda0484e03ea2b665c8146bae7467e68f0
phen97 <pedro.hvs@outlook.com.br>
2023-06-13 11:06:19 +0100

    adding scripts and style

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+const noWhitespace = document.getElementById("noWhitespace").innerHTML;
+console.log(noWhitespace);
+// "Dogs are silly."
+
+const whitespace = document.getElementById("whitespace").innerHTML;
+console.log(whitespace);
+// "Dogs
+//    are
+//        silly."
