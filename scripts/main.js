eeffd4959d8b95d894221a35c2e1715a9981bd6e
phen97 <pedro.hvs@outlook.com.br>
2023-06-16 09:21:36 +0100

    add page "photos" plus 2 photos. update index

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-const noWhitespace = document.getElementById("noWhitespace").innerHTML;
-console.log(noWhitespace);
-// "Dogs are silly."
-
-const whitespace = document.getElementById("whitespace").innerHTML;
-console.log(whitespace);
-// "Dogs
-//    are
-//        silly."
