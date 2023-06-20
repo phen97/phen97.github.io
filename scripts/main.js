777964b97e3f9a9ee7fbf873c695a4effc2a3215
phen97 <pedro.hvs@outlook.com.br>
2023-06-20 23:50:59 +0100

    manymany changes

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+function goLight() {
+    var element = document.body;
+    element.classList.toggle("light-mode");
+    var element = document.getElementById("logo");
+    element.classList.toggle("light-mode-svg");
+ }
